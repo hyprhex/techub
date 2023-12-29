e8fa062cda80c3d070717fd2fe13caae22beecc4
HyprHex <swapapis@gmail.com>
2023-12-29 15:12:53 +0100

    Get jobs Id

--- main.go
+++ main.go
@@ -1,5 +1,52 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+const (
+	jobUrl     = "https://hacker-news.firebaseio.com/v0/jobstories.json"
+	jobItemUrl = "https://hacker-news.firebaseio.com/v0/item/%s.json"
+)
+
+func getJobsId(url string) ([]int, error) {
+
+	res, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+
+	var resIds []int
+
+	if res.StatusCode == http.StatusOK {
+		resData, err := io.ReadAll(res.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		err = json.Unmarshal(resData, &resIds)
+
+		if err != nil {
+			return nil, err
+		}
+
+	}
+
+	return resIds, nil
+}
+
 func main() {
+	res, err := getJobsId(jobUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range res {
+		fmt.Println(v)
+	}
 
 }
